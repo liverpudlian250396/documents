d559e14045dff243c0874027ee8b420ae58b5e94
liverpudlian250396 <54277267+liverpudlian250396@users.noreply.github.com>
2019-10-08 21:14:52 +0700

    update

---
+++ 19JSP04/bin/html/demo.js
@@ -0,0 +1,12 @@
+var a,b;
+function msg() {
+       a = prompt("input: ");
+       v = prompt("input: ");
+       var s = parstInt(a) + parstInt(b);
+       if (s % 2 == 0) {
+       	alert("so chan");
+       }
+       else{
+       		alert("so le");
+       }      	
+    }
